8f62e8d87fab0086be0665d68c79cfa8c3708485
Enrico <54644759+Favstvs@users.noreply.github.com>
2023-05-01 14:13:06 +0000

    Add files via upload

---
+++ daily/userdaily.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `ID_User` bigint NOT NULL,
+  `Username` varchar(36) NOT NULL,
+  `Coins` bigint NOT NULL,
+  PRIMARY KEY (`ID_User`)
+) ENGINE=InnoDB;
