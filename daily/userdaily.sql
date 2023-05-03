f0c048de5c585f034c355eab27ad495360d44f6a
Enrico <54644759+Favstvs@users.noreply.github.com>
2023-05-03 14:47:46 +0000

    Update userdaily.sql

--- daily/userdaily.sql
+++ daily/userdaily.sql
@@ -4,5 +4,6 @@ CREATE TABLE `users` (
   `ID_User` bigint NOT NULL,
   `Username` varchar(36) NOT NULL,
   `Coins` bigint NOT NULL,
+  KEY `Coins` (`Coins`),
   PRIMARY KEY (`ID_User`)
 ) ENGINE=InnoDB;
