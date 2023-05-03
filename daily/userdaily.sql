f348bbaf1b08c6e04d93814b66ff4a2af63564f7
Enrico <54644759+Favstvs@users.noreply.github.com>
2023-05-03 14:53:04 +0000

    Update userdaily.sql

--- daily/userdaily.sql
+++ daily/userdaily.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS `daily`;
 CREATE TABLE `daily` (
   `ID_User` bigint NOT NULL,
   `Username` varchar(36) NOT NULL,
-  `Coins` int NOT NULL AUTO_INCREMENT ,
+  `Coins` int NOT NULL AUTO_INCREMENT,
   KEY `Coins` (`Coins`),
   PRIMARY KEY (`ID_User`)
 ) ENGINE=InnoDB;
