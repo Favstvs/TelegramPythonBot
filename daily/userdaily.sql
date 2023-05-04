2cc23367dfd83ca9b0eb9882e3f7b8ee65e2729a
Enrico <54644759+Favstvs@users.noreply.github.com>
2023-05-04 16:30:16 +0000

    Delete userdaily.sql

--- daily/userdaily.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `daily`;
-
-CREATE TABLE `daily` (
-  `ID_User` bigint NOT NULL,
-  `Username` varchar(36) NOT NULL,
-  `Coins` int NOT NULL AUTO_INCREMENT,
-  KEY `Coins` (`Coins`),
-  PRIMARY KEY (`ID_User`)
-) ENGINE=InnoDB;
