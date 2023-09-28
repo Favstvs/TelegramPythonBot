aa76f76e78b2892467b1033765baca7ae7a01ace
Enrico <54644759+Favstvs@users.noreply.github.com>
2023-09-28 22:49:29 +0200

    Update and rename management.sql to hmanagement.sql

---
+++ SEP2023/packs/SQLMod/HSQL/hmanagement.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS `hmanagement`;
+
+CREATE TABLE `hmanagement` (
+  `ID_Supergruppo` bigint NOT NULL,
+  `ID_Husbando` int DEFAULT NULL,
+  `Time_Mess` int unsigned NOT NULL,
+  `Started` tinyint NOT NULL,
+  `Time_reset` int unsigned NOT NULL,
+  PRIMARY KEY (`ID_Supergruppo`),
+  KEY `management_ibfk_2` (`ID_Husbando`),
+  CONSTRAINT `management_ibfk_1` FOREIGN KEY (`ID_Supergruppo`) REFERENCES `supergruppo` (`ID_Supergruppo`),
+  CONSTRAINT `management_ibfk_2` FOREIGN KEY (`ID_Husbando`) REFERENCES `husbandi` (`ID_Husbando`)
+) ENGINE=InnoDB;
