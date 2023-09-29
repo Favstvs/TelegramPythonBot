ef793930b28e40952062443c0b73e8acd68c0807
Enrico <54644759+Favstvs@users.noreply.github.com>
2023-09-29 17:22:50 +0200

    Update H&Wmanagement.sql

--- SEP2023/packs/SQLMod/H&WSQL/H&Wmanagement.sql
+++ SEP2023/packs/SQLMod/H&WSQL/H&Wmanagement.sql
@@ -1,1 +1,19 @@
+DROP TABLE IF EXISTS `H&Wmanagement`;
 
+CREATE TABLE `H&Wmanagement` (
+  `ID_Supergruppo` bigint NOT NULL,
+  `ID_Waifu` int DEFAULT NULL,
+  `ID_Husbando` int DEFAULT NULL,
+  `Time_Mess` int unsigned NOT NULL,
+  `Started` tinyint NOT NULL,
+  `Time_reset` int unsigned NOT NULL,
+  PRIMARY KEY (`ID_Supergruppo`),
+  KEY `management_ibfk_2` (`ID_Waifu`),
+  KEY `management_ibfk_3` (`ID_Husbando`),
+  CONSTRAINT `management_ibfk_1` FOREIGN KEY (`ID_Supergruppo`) REFERENCES `supergruppo` (`ID_Supergruppo`),
+  CONSTRAINT `management_ibfk_2` FOREIGN KEY (`ID_Waifu`) REFERENCES `waifu` (`ID_Waifu`),
+  CONSTRAINT `management_ibfk_3` FOREIGN KEY (`ID_Husbando`) REFERENCES `husbando` (`ID_Husbando`)
+
+  
+  
+) ENGINE=InnoDB;
