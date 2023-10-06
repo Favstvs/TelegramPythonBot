19918770cc39e940d7a384c7c5c8684c94e44159
Enrico <54644759+Favstvs@users.noreply.github.com>
2023-10-06 13:25:20 +0200

    Update users.sql

--- SEP2023/packs/SQLMod/H&WSQL/users.sql
+++ SEP2023/packs/SQLMod/H&WSQL/users.sql
@@ -3,5 +3,8 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `ID_User` bigint NOT NULL,
   `Username` varchar(36) NOT NULL,
+  `Coins` int NOT NULL AUTO_INCREMENT,
+  `Time_Mess` datetime,
+  KEY `Coins` (`Coins`),
   PRIMARY KEY (`ID_User`)
 ) ENGINE=InnoDB;
