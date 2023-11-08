8a0fb07e797be197892128b90145bf122302bb07
Enrico <54644759+Favstvs@users.noreply.github.com>
2023-11-08 13:50:49 +0100

    Update HWTelBot.py

--- NOV23/HWTelBot.py
+++ NOV23/HWTelBot.py
@@ -2264,6 +2264,9 @@ def UpdatePacks(update: Update, context: CallbackContext):
                          InlineKeyboardButton('HUSBANDO', callback_data='Husbando@Tel_Bot')],
                          [InlineKeyboardButton('Quit', callback_data='Esci@Tel_Bot')]]
             reply_markup = InlineKeyboardMarkup(keyboard)
+          
+           #PROVA reply_markup prima di inviare il messagio dei packs
+           #context.bot.send_photo(chat_id=ID_Supergruppo, photo=open('/home/enrico/Immagini/w&h.jpeg', 'rb'), caption="Hey Hey <b>Appare finalmente un pack</b> 📦\n Scegli se vuoi <i>waifu</i> oppure <i>husbando<i>\n", reply_markup=reply_markup, parse_mode='HTML')
             # Raccolgo la richiesta del callback
             if CallBackRequest == "Waifu@Tel_Bot":
                 for x in range(3):
